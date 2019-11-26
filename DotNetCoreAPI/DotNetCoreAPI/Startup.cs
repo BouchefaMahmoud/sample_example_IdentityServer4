56ddd5f37fee21f545e94135090a16e6ed0355fa
Mahmoud BOUCHEFA <Mahmoud.BOUCHEFA@bluesoft-group.com>
2019-11-26 15:46:11 +0100

    first commit

---
+++ DotNetCoreAPI/DotNetCoreAPI/Startup.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityServer4.AccessTokenValidation;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace IdentityServerTest.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+
+        /*Immédiatement après l' AddMvcCore ajout du service d'autorisation à la collection de services avec AddAuthorization () .
+         Vous pouvez également utiliser la AuthorizationOptions pour configurer une autorisation basée sur des revendications ou une stratégie .*/
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services
+                .AddMvcCore(options =>
+                {
+                    // demanderl'autorisation pour utiliser chaque fonction du controller 
+                    options.Filters.Add(new AuthorizeFilter());
+                }) 
+                .AddJsonFormatters()
+                .AddAuthorization()
+                .SetCompatibilityVersion(CompatibilityVersion.Latest);
+            /*
+                Après cette configuration de base, vous devez configurer l'authentification. Vous faites cela avec la méthode AddAuthentication() .
+                Étant donné que nous allons utiliser un jeton porteur OAuth 2.0 , 
+                nous devons également le transmettre à la méthode. Ensuite, 
+                nous ajoutons la configuration de l’authentification du serveur d’identité avec la méthode
+                AddIdentityServerAuthentication() , dans laquelle nous devons définir l’URL de IdentityServer, le ApiName et bien sûr le secret
+             */
+
+            services.AddAuthentication(IdentityServerAuthenticationDefaults.AuthenticationScheme)
+                    .AddIdentityServerAuthentication(options =>
+                    {
+                        options.Authority = "http://localhost:5000";
+                        options.RequireHttpsMetadata = false;
+
+                        options.ApiName = "ApiName";
+                        options.ApiSecret = "secret_for_the_api";
+                    });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+
+            /*
+                Étape la plus facile! Ajoutez simplement AuthenticationMiddleware dans le pipeline,
+                mais faites attention où! Ce doit être avant tous les autres middlewares, qui est la première dans le pipeline
+                pipeLine : la ou on met les middlwars qui gèrent les requettes HTTP
+             */
+            app.UseAuthentication();
+
+
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
---
+++ IdentityServer4/TokenService/Config.cs
@@ -0,0 +1,90 @@
+﻿
+/*
+ Identity Server dispose de trois entités principales que nous devons configurer : le ApiResource, le Clientet le TestUser. 
+ Tous auront besoin d’une configuration minimale, mais avant de commencer, il est utile de garder à l’esprit les points suivants:
+
+-L’ application console jouera le rôle de Client. Il utilise un ClientId & a Secret plus le nom d'utilisateur et le mot de passe d'un utilisateur pour obtenir le jeton.
+-L’API ASP.NET Core sera bien sûr le ApiResource. Il utilise un ApiName & Secret plus le jeton d'accès, pour Claims .
+-Un Client doit avoir un ApiResource dans leur AllowedScopes liste pour que le serveur Idenity permet l'accès
+ 
+ 
+ */
+
+using IdentityModel;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace IdentityServer
+{
+
+
+
+
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new IdentityResource[]
+            {
+                new IdentityResources.OpenId()
+            };
+        }
+
+        public static IEnumerable<ApiResource> GetApis()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("ApiName")
+                {
+                    ApiSecrets = {new Secret("secret_for_the_api".Sha256())}
+                }
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "ConsoleApp_ClientId",
+                    ClientSecrets = { new Secret("secret_for_the_consoleapp".Sha256()) },
+                    AccessTokenType = AccessTokenType.Reference,
+
+                    //GrantTypes.ResourceOwnerPassword signifie simplement permettre à un client d'envoyer un nom d'utilisateur 
+                    //et un mot de passe au service de jetons et de récupérer un jeton d'accès représentant cet utilisateur
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+                    // la liste des api auquelles il peut accéder 
+                    AllowedScopes = { "ApiName" },
+                    
+                }
+            };
+        }
+
+
+
+        /*
+            toutes les méthodes de Config.cs sont appelées à partir de StartUp.cs 
+            lors de la configuration du service, et nous suivrons ce "modèle" pour la TestUsers méthode  
+        */
+
+        public static List<TestUser> GetTestUsers()
+        {
+            return new List<TestUser>()
+            {
+                new TestUser
+                {
+                    SubjectId = "1",
+                    Username = "123",
+                    Password = "123".Sha256(),
+                    //Claims =
+                    //{
+                    //    new Claim(JwtClaimTypes.Role, "SomeRole")
+                    //}
+                }
+            };
+        }
+    }
+}
